fb1b73c007a13df39c4db084943cd0cf609005c3
Davron <152484311+Davron030901@users.noreply.github.com>
2024-03-21 16:12:43 +0500

    Update ThreeCounPlus.py

---
+++ ThreeCounPlus.py
@@ -0,0 +1,11 @@
+numer1=int(input("Enter number1: "))
+numer2=int(input("Enter number2: "))
+numer3=int(input("Enter number3: "))
+count=0
+if numer1>0:
+    count+=1
+if numer2>0:
+    count+=1
+if numer3>0:
+    count+=1
+print(count)
